9a967e612db10960066984eb0df0d51e56cb7a24
romain Orioli <orioli62193@gmail.com>
2025-11-28 16:51:53 +0000

    avancée de l'appli

--- app.R
+++ app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(DT)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -15,28 +16,42 @@ ui <- fluidPage(
                         label="Height of characters:",
                         min = 0,
                         max = 250,
-                        value = 100)
+                        value = 100), 
+            selectInput(inputId="gender",
+                        label="character's gender",c("masculine", "feminine"),selected = NULL, multiple = FALSE, selectize = TRUE)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput(outputId="StarWarsPlot")
+           textOutput(outputId = "nbtext"),
+           plotOutput(outputId="StarWarsPlot"),
+           DTOutput(outputId="StarwarsDT")
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+  
+    output$nbtext<-renderText({paste(starwars |> 
+        filter(height > input$taille, gender==input$gender) |> nrow()," personnages concernés")
+    })
+  
     output$StarWarsPlot <- renderPlot({
       starwars |> 
-        filter(height > input$taille) |>
+        filter(height > input$taille & gender==input$gender) |> 
         ggplot(aes(x = height)) +
         geom_histogram(
           binwidth = 10, 
           fill = "blue", 
           color = "white"
-        )
+        ) +
+        labs(title=paste("Taille pour les personnes ", input$gender,"mesurant plus de",input$taille, " cm" ))
+    })
+    
+    output$StarwarsDT<-renderDT({
+      starwars|> 
+        filter(height > input$taille & gender==input$gender)|> select(name) 
     })
 }
 
